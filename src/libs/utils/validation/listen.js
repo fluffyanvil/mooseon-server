d301f6a653cc2895d55f01a45357778c2c33620c
sergey.garnov <sergeygarnov1991@gmail.com>
2018-08-09 10:39:31 +0300

    geo coord range

--- src/libs/utils/validation/listen.js
+++ src/libs/utils/validation/listen.js
@@ -2,11 +2,11 @@ const Joi = require('joi');
 
 module.exports = {
     body: {
-        user: Joi.string().alphanum().min(3).max(30).required(),
-        lng: Joi.number().required(),
-        lat: Joi.number().required(),
-        artist: Joi.string().alphanum().required(),
-        track:  Joi.string().alphanum().required()
+        user: Joi.string().min(3).max(30).required(),
+        lng: Joi.number().min(-180).max(180).required(),
+        lat: Joi.number().min(-85.05112878).max(85.05112878).required(),
+        artist: Joi.string().required(),
+        track:  Joi.string().required()
     }
   };
 
